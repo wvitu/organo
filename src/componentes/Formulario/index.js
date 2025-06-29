11956536b0ca2c3408efb3e800c8c9299dce2f17
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-06-29 14:48:20 -0300

    Controlando o estado
    Controla inputs do formulário com useState e eleva estado ao componente pai

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,6 +2,8 @@ import './Formulario.css';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
+import { useState } from 'react';
+
 
 const Formulario = () => {
 
@@ -18,6 +20,8 @@ const Formulario = () => {
     const [nome, setNome] = useState('');
     const [cargo, setCargo] = useState('');
     const [imagem, setImagem] = useState('');
+    const [time, setTime] = useState('');
+
 
     const aoSalvar = (evento) => {
         evento.preventDefault();
@@ -28,27 +32,33 @@ const Formulario = () => {
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto 
-                obrigatorio={true} 
-                label="Nome" 
-                placeholder="Digite seu nome"
-                valor={nome}
-                aoAlterado={valor => setNome(nome)} 
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(nome)}
                 />
-                <CampoTexto 
-                obrigatorio={true} 
-                label="Cargo" 
-                placeholder="Digite seu cargo" 
-                valor={cargo}
-                aoAlterado={valor => setCargo(cargo)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(cargo)}
                 />
-                <CampoTexto 
-                label="Imagem" 
-                placeholder="Digite o endereço da imagem" 
-                valor={imagem}
-                aoAlterado={valor => setImagem(imagem)}
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(imagem)}
                 />
-                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <ListaSuspensa
+                    obrigatorio={true}
+                    label="Time"
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(time)}
+                />
                 <Botao>
                     Criar Card
                 </Botao>
