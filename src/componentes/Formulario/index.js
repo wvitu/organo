10714029ace6ebb708c071ea5ce9aa47f7a9f091
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-07-01 21:49:18 -0300

    Refatorando código duplicado
    Times apenas no app e buscando no index do Formulário a lista de times através de Maps

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -7,16 +7,6 @@ import { useState } from 'react';
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão',
-    ]
-
     const [nome, setNome] = useState('');
     const [cargo, setCargo] = useState('');
     const [imagem, setImagem] = useState('');
@@ -61,7 +51,7 @@ const Formulario = (props) => {
                 <ListaSuspensa
                     obrigatorio={true}
                     label="Time"
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(time)}
                 />
