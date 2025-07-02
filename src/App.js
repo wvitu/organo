833b146eec51aab1222a1d4d7fe886e33525e9aa
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-07-01 21:15:02 -0300

    Elevando a lista de times
    Adicionando propriedades de cores primárias e secundárias de Times e criando um objeto no App para adicionar dinamicamente

--- src/App.js
+++ src/App.js
@@ -6,6 +6,45 @@ import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
+
+
   const [colaboradores, setColaboradores] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
@@ -17,15 +56,8 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
-      <Time nome="Programação" />
-      <Time nome="Front-End" />
-      <Time nome="Data Science" />
-      <Time nome="Devops" />
-      <Time nome="UX e Design" />
-      <Time nome="Mobile" />
-      <Time nome="Inovação e Gestão" />
-      <Time nome="Outros" />
 
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
      
     </div>
   );
