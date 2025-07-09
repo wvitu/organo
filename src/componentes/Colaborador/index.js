d8dae458f6ef470213a50a533195f47540cfd067
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-07-09 19:58:10 -0300

    Card colaborador criado, css adicionado

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return ( <div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/wvitu.png' alt='Wanderson Vitor' />
+        </div>
+        <div className='rodape'>
+            <h4>Wanderson Vitor</h4>
+            <h5>Front-End</h5>
+        </div>
+    </div>)
+}
+       
+    
+
+    export default Colaborador
